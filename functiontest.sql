a8061af1d2fc1c7d47c6b3c8b1a155a9fed9c10a
ĐÀO ĐỨC PHONG <162095979+nielday@users.noreply.github.com>
2025-03-19 21:05:33 +0700

    Add files via upload

---
+++ functiontest.sql
@@ -0,0 +1,45 @@
+﻿												--TestFunction--
+USE [QLDatGiaoHang];
+GO
+
+--fn_GetTotalOrderedQuantity
+SELECT dbo.fn_GetTotalOrderedQuantity(101) AS TotalOrdered;
+
+--fn_SupplierExists
+IF dbo.fn_SupplierExists(2) = 1
+    PRINT 'Nhà cung cấp tồn tại.';
+ELSE
+    PRINT 'Nhà cung cấp không tồn tại.';
+
+
+--fn_CheckInventory
+SELECT dbo.fn_CheckInventory(101) AS Inventory;
+
+
+--fn_GetProductsBySupplier
+SELECT * FROM dbo.fn_GetProductsBySupplier(1);
+
+--fn_GetOrdersByDateRange
+SELECT * FROM dbo.fn_GetOrdersByDateRange('2023-10-01', '2023-10-10');
+
+
+--fn_GetTopProducts
+SELECT * FROM dbo.fn_GetTopProducts(4);
+
+
+--fn_GetDeliveryDetails
+SELECT * FROM dbo.fn_GetDeliveryDetails(501);
+
+
+--fn_GetSupplierRevenue
+SELECT * FROM dbo.fn_GetSupplierRevenue();
+
+--sp_GetProductSummary
+EXEC sp_GetProductSummary @ProductID = 101;
+
+
+--fn_SupplierExists
+-- Kiểm tra nhà cung cấp có SupplierID = 1(có tồn tại)
+SELECT dbo.fn_SupplierExists(1) AS SupplierExists;
+-- Kiểm tra nhà cung cấp có SupplierID = 999(ko tồn tại)
+SELECT dbo.fn_SupplierExists(999) AS SupplierExists;
---
+++ mahoa.sql
@@ -0,0 +1,74 @@
+﻿--Tạo Master Key, Certificate và Symmetric Key
+-- Tạo Database Master Key (DMK)
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'YourSecurePassword123!';
+
+-- Tạo Certificate
+CREATE CERTIFICATE CustomerEncryptCert 
+WITH SUBJECT = 'Encrypt Customer Sensitive Data';
+
+-- Tạo Symmetric Key dùng AES 256
+CREATE SYMMETRIC KEY CustomerAESKey
+WITH ALGORITHM = AES_256
+ENCRYPTION BY CERTIFICATE CustomerEncryptCert;
+--Thêm cột mới kiểu VARBINARY để lưu dữ liệu mã hóa
+-- Thêm cột mới
+ALTER TABLE Customer
+ADD 
+    Email_Encrypted VARBINARY(256),
+    Phone_Encrypted VARBINARY(256),
+    Address_Encrypted VARBINARY(256);
+--Mã hóa dữ liệu hiện có
+-- Mở Symmetric Key
+OPEN SYMMETRIC KEY CustomerAESKey
+DECRYPTION BY CERTIFICATE CustomerEncryptCert;
+
+-- Cập nhật dữ liệu mã hóa
+UPDATE Customer
+SET 
+    Email_Encrypted = ENCRYPTBYKEY(KEY_GUID('CustomerAESKey'), Email),
+    Phone_Encrypted = ENCRYPTBYKEY(KEY_GUID('CustomerAESKey'), Phone),
+    Address_Encrypted = ENCRYPTBYKEY(KEY_GUID('CustomerAESKey'), Address);
+
+-- Đóng Symmetric Key
+CLOSE SYMMETRIC KEY CustomerAESKey;
+--Giải mã dữ liệu để xác nhận nội dung gốc
+
+-- Mở Symmetric Key
+OPEN SYMMETRIC KEY CustomerAESKey
+DECRYPTION BY CERTIFICATE CustomerEncryptCert;
+
+-- Truy vấn dữ liệu giải mã
+SELECT 
+    CustomerID,
+    CustomerName,
+    CONVERT(NVARCHAR, DECRYPTBYKEY(Email_Encrypted)) AS Email,
+    CONVERT(VARCHAR(20), DECRYPTBYKEY(Phone_Encrypted)) AS Phone,
+    CONVERT(NVARCHAR, DECRYPTBYKEY(Address_Encrypted)) AS Address
+FROM Customer;
+
+-- Đóng Symmetric Key
+CLOSE SYMMETRIC KEY CustomerAESKey;
+
+
+--Truy vấn dữ liệu đã mã hóa (dạng nhị phân)
+-- Xem dữ liệu mã hóa (dạng VARBINARY)
+SELECT 
+    CustomerID,
+    Email_Encrypted AS EncryptedEmail,
+    Phone_Encrypted AS EncryptedPhone,
+    Address_Encrypted AS EncryptedAddress
+FROM Customer;
+Backup Certificate và Private Key
+BACKUP CERTIFICATE CustomerEncryptCert 
+TO FILE = 'C:\Security\CustomerEncryptCert.cer'
+WITH PRIVATE KEY (
+    FILE = 'C:\Security\CustomerEncryptCert.pvk',
+    ENCRYPTION BY PASSWORD = 'YourBackupPassword123!'
+);
+--Khôi phục khóa
+CREATE CERTIFICATE CustomerEncryptCert 
+FROM FILE = 'C:\Security\CustomerEncryptCert.cer'
+WITH PRIVATE KEY (
+    FILE = 'C:\Security\CustomerEncryptCert.pvk',
+    DECRYPTION BY PASSWORD = 'YourBackupPassword123!'
+);
